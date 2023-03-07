bd3388a994e286875df3d5b696a32a25a582b858
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-03-07 00:44:34 -0300

    feat: basic structure of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import "./app.module.css";
 
 function App() {
+  const [showGame, setShowGame] = useState<boolean>(false);
+  const [repos, setRepos] = useState([1])
+
+  const handleStartGame = () => {
+    setShowGame(true)
+  };
 
   return (
-    <div className=""> 
-    Hello    
-    </div>
+    <main>
+      {!showGame ? (
+        <>
+          <h1>Welcome to Repo-Wars</h1>
+          <h2>A developer-driven guessing game</h2>
+          {!repos.length ? (
+            <span>Loading game...</span>
+          ) : (
+            <button
+              type="button"
+              onClick={handleStartGame}
+            >
+              Start
+            </button>
+          )}
+        </>
+      ) : (
+        <h2>Jogo</h2>
+      )} 
+    </main>
   )
 }
 
