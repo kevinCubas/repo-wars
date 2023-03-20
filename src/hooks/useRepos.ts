c778efa1436a2d2fc6cdedd3f8b9b4f689cef8c3
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-03-20 16:23:07 -0300

    fix: attribute allRepos and implement fetch request

--- src/hooks/useRepos.ts
+++ src/hooks/useRepos.ts
@@ -1,7 +1,23 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react"
 import { IRepo } from "../@types/repo";
 import { githubClient } from "../api"
+
 const randomSince = Math.floor(Math.random() * 1000000);
+
+const fetchRepos = async (
+  amount: number,
+  setters: Dispatch<SetStateAction<any[]>>[],
+) => {
+  const res = await githubClient.request("GET /search/repositories", {
+    q: `is:public stars:>1000 fork:false`,
+    sort: "stars",
+    order: "desc",
+    per_page: amount,
+    since: randomSince,
+    page: 1,
+  })
+  setters.forEach((setter) => setter(res.data.items))
+}
 
 type UseReposReturnType = [
   repositories: IRepo[],
@@ -14,18 +30,7 @@ export const useRepos = (amount: number): UseReposReturnType => {
   const [repositories, setRepositories] = useState<IRepo[]>([])
 
   useEffect(() => {
-    githubClient
-      .request("GET /search/repositories", {
-        q: `is:public stars:>1000 fork:false`,
-        sort: "stars",
-        order: "desc",
-        per_page: amount,
-        since: randomSince,
-        page: 1,
-      })
-      .then((response) => {
-        setRepositories(response.data.items as IRepo[]);
-      });
+    fetchRepos(amount, [setAllRepos, setRepositories])
   }, [])
 
   return [repositories, setRepositories, allRepos]
